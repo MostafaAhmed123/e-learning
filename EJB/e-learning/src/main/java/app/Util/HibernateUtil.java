db59d1888cadee09b84828666f59ba2e7718cff8
Mostafa Ahmed <m.a7med82002@gmail.com>
2024-05-04 21:48:42 +0300

    clean up

--- EJB/e-learning/src/main/java/app/Util/HibernateUtil.java
+++ EJB/e-learning/src/main/java/app/Util/HibernateUtil.java
@@ -1,17 +1,11 @@
 package app.Util;
 
-import java.util.Properties;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
-
-import app.Models.Instructors;
-import app.Models.Students;
-import app.Models.Users;
+import app.Models.*;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
