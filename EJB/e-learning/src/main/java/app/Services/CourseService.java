773f0b15c7c31da4c81d402db1f41ce75d935079
Mostafa Ahmed <m.a7med82002@gmail.com>
2024-05-06 22:19:08 +0300

    adding some bugs

--- EJB/e-learning/src/main/java/app/Services/CourseService.java
+++ EJB/e-learning/src/main/java/app/Services/CourseService.java
@@ -6,6 +6,7 @@ import javax.ejb.Stateless;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.hibernate.Transaction;
@@ -31,12 +32,12 @@ public class CourseService {
             if (session != null)
                 session.close();
         }
-        return course;
+        return course.isApprovedByAdmin() ? course : null;
     }
 
     private boolean isCourseExist(String name) {
         Session session = HibernateUtil.getSession();
-        String hql = "FROM Courses WHERE name = :name";
+        String hql = "FROM Courses WHERE name = :name AND c.approvedByAdmin == true";
         Query<Courses> query = session.createQuery(hql, Courses.class);
         query.setParameter("name", name);
         return query.getResultList().size() > 0;
@@ -69,11 +70,11 @@ public class CourseService {
             String hql;
             Query<Courses> query;
             if (byName) {
-                hql = "FROM Courses c WHERE c.name LIKE :searchTerm";
+                hql = "FROM Courses c WHERE c.name LIKE :searchTerm AND c.approvedByAdmin == true";
                 query = session.createQuery(hql, Courses.class);
                 query.setParameter("searchTerm", searchTerm);
             } else {
-                hql = "FROM Courses c WHERE c.category LIKE :searchTerm";
+                hql = "FROM Courses c WHERE c.category LIKE :searchTerm AND c.approvedByAdmin == true";
                 query = session.createQuery(hql, Courses.class);
                 query.setParameter("searchTerm", app.Util.Enums.Category.valueOf(searchTerm));
             }
@@ -91,7 +92,7 @@ public class CourseService {
         Session session = HibernateUtil.getSession();
         List<Courses> courses = null;
         try {
-            Query<Courses> query = session.createQuery("FROM Courses", Courses.class);
+            Query<Courses> query = session.createQuery("FROM Courses c WHERE c.approvedByAdmin == true", Courses.class);
             courses = query.getResultList();
         } catch (Exception e) {
             System.out.println("Error in Browsing " + e.getMessage());
@@ -107,7 +108,9 @@ public class CourseService {
             CriteriaQuery<Courses> cq = cb.createQuery(Courses.class);
             Root<Courses> courseRoot = cq.from(Courses.class);
             Join<Courses, Reviews> reviewsJoin = courseRoot.join("courseReviews");
-            cq.select(courseRoot).groupBy(courseRoot.get("courseId"))
+            Predicate approvedPredicate = cb.isTrue(courseRoot.get("approvedByAdmin"));
+            cq.select(courseRoot).where(approvedPredicate)
+                    .groupBy(courseRoot.get("courseId"))
                     .orderBy(cb.desc(cb.avg(reviewsJoin.get("rating"))));
             return session.createQuery(cq).getResultList();
         } catch (Exception e) {
