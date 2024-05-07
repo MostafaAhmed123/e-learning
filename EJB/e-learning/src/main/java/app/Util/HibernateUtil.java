88792e9fe3c8e888bfc69ebafe0e8cccae9f4c1c
Mostafa Ahmed <m.a7med82002@gmail.com>
2024-05-07 20:59:38 +0300

    adding singleton EJB

--- EJB/e-learning/src/main/java/app/Util/HibernateUtil.java
+++ EJB/e-learning/src/main/java/app/Util/HibernateUtil.java
@@ -1,5 +1,7 @@
 package app.Util;
 
+import javax.ejb.Singleton;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -7,11 +9,12 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import app.Models.*;
 
+@Singleton
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
 
     public static Session getSession() {
-        if (sessionFactory == null) {
+        if (sessionFactory == null || sessionFactory.isClosed()) {
             try {
                 Configuration configuration = new Configuration();
                 configuration.configure("hibernate.cfg.xml");
@@ -28,8 +31,21 @@ public class HibernateUtil {
                 sessionFactory = configuration.buildSessionFactory(serviceRegistry);
             } catch (Exception e) {
                 e.printStackTrace();
+                System.out.println(e.getMessage());
             }
         }
         return sessionFactory.openSession();
+    }
+
+    public static void closeSession(Session session) {
+        if (session != null && session.isOpen()) {
+            session.close();
+        }
+    }
+
+    public static void closeSessionFactory() {
+        if (sessionFactory != null && !sessionFactory.isClosed()) {
+            sessionFactory.close();
+        }
     }
 }
